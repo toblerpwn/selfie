e68b536b5c3c4f2a14bbf282c72dea1e8ad2f86c
Sean Conrad <toblerpwn@gmail.com>
2013-06-25 16:23:41 -0700

    initial commit

---
+++ Podfile
@@ -0,0 +1,4 @@
+platform :ios, '6.1'
+pod 'FlatUIKit', '~> 1.1'
+pod 'MBProgressHUD', '0.6'
+pod 'UIColor+MLPFlatColors', '1.0'
---
+++ Pods/Pods-environment.h
@@ -0,0 +1,26 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// FlatUIKit
+#define COCOAPODS_POD_AVAILABLE_FlatUIKit
+#define COCOAPODS_VERSION_MAJOR_FlatUIKit 1
+#define COCOAPODS_VERSION_MINOR_FlatUIKit 1
+#define COCOAPODS_VERSION_PATCH_FlatUIKit 0
+
+// MBProgressHUD
+#define COCOAPODS_POD_AVAILABLE_MBProgressHUD
+#define COCOAPODS_VERSION_MAJOR_MBProgressHUD 0
+#define COCOAPODS_VERSION_MINOR_MBProgressHUD 6
+#define COCOAPODS_VERSION_PATCH_MBProgressHUD 0
+
+// UIColor+MLPFlatColors
+#define COCOAPODS_POD_AVAILABLE_UIColor_MLPFlatColors
+#define COCOAPODS_VERSION_MAJOR_UIColor_MLPFlatColors 1
+#define COCOAPODS_VERSION_MINOR_UIColor_MLPFlatColors 0
+#define COCOAPODS_VERSION_PATCH_UIColor_MLPFlatColors 0
+

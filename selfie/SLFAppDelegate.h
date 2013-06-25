75b21a2e0fde47b0985f7cdafad3607a7e731a58
Sean Conrad <toblerpwn@gmail.com>
2013-06-25 16:11:48 -0700

    Initial Commit

---
+++ selfie/SLFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SLFAppDelegate.h
+//  selfie
+//
+//  Created by Sean Conrad on 6/25/13.
+//  Copyright (c) 2013 Sean Conrad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SLFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ selfie/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  selfie
+//
+//  Created by Sean Conrad on 6/25/13.
+//  Copyright (c) 2013 Sean Conrad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SLFAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SLFAppDelegate class]));
+    }
+}
